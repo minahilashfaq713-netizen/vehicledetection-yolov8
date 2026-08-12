7b87ee1cbf2581ad5c2614176f1dbb7ab77965af
minahilashfaq713-netizen <minahilashfaq713@gmail.com>
2026-08-12 15:09:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.set_page_config(
     layout="wide",
 )
 
-   MODEL_PATH = "yolov8n.pt"
+   MODEL_PATH = Path(__file__).parent / "weights" / "best.pt"
 
 
 # --------------------------------------------------------------------------
